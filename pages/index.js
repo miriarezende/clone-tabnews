7952a06a8341dfa0077272de90ea031edc1e1356
Miriã Gonçalves <111203584+miriarezende@users.noreply.github.com>
2023-07-03 21:20:48 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home(){
+    return <p>
+        muito entendo
+        <br/>uma barganha envolvida
+        <br/>mal me entendo
+        <br/>e nem comece hipocrisias envolver
+        <br/>dicas envolvidas basta entender 
+        <br/>inutilidade já inicia na humanidade 
+        <br/>outro ser qualitativo 
+        <br/>usuario a nascer
+        <br/>dito o merecido envenenadado vou entorpecer
+        <br/>retorno, todo estorno 
+        <br/>ademais deve ocorrer 
+        <br/>relembre ouvinte!!
+    </p>
+}
+
+export default Home;
